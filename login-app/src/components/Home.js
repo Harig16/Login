4644b5687690569061979bceb37839c6e9e8cbef
Harig16 <95hari.gopal@gmail.com>
2022-08-02 09:49:49 +0530

    Add login form

--- login-app/src/components/Home.js
+++ login-app/src/components/Home.js
@@ -1,6 +1,12 @@
 import React from "react";
+import { Navigate } from "react-router-dom";
 
 const Home = (props) => {
+
+  if(props.isLogged ===  "Not_Logged_in") {
+    return <Navigate to="/login" replace={true} />
+  }
+
   return (
     <div>
       <h1>Hello from Home Component</h1>
--- login-app/src/components/Login.js
+++ login-app/src/components/Login.js
@@ -1,11 +1,49 @@
 import React from "react";
 
-const Login = () => {
-  return (
-    <div>
-      <h1>render from Login components</h1>
-    </div>
-  )
+class Login extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {
+        username: "",
+        password: "",
+      },
+      inputValue: "",
+    };
+    this.inputHandler = this.inputHandler.bind(this);
+  }
+
+  inputHandler = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  render () {
+    return (
+      <div>
+        <h1>Login Form</h1>
+        <div>
+          <label>Username: </label>
+          <input 
+            type="text"
+            name="user['username']"
+            value={this.state.username}
+            onChange={this.inputHandler}
+          />
+          <label>Password: </label>
+          <input 
+            type="password"
+            name="user['password']"
+            value={this.state.password}
+            onChange={this.inputHandler}
+          />
+          <button type="submit">Login</button>
+        </div>
+      </div>
+    )
+  } 
 }
 
 export default Login;
