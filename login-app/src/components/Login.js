0313b00ccd269c7398fb2097bcc1e9580c56b828
Harig16 <95hari.gopal@gmail.com>
2022-08-02 12:46:27 +0530

    Remove login handler

--- login-app/src/components/Login.js
+++ login-app/src/components/Login.js
@@ -1,17 +1,17 @@
 import React from "react";
+import axios from 'axios';
 
 class Login extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      user: {
-        username: "",
-        password: "",
-      },
+      username: "",
+      password: "",
       inputValue: "",
     };
     this.inputHandler = this.inputHandler.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   inputHandler = (e) => {
@@ -20,26 +20,44 @@ class Login extends React.Component {
     })
   }
 
+  handleSubmit = (e) => {
+    const { username, password } = this.state;
+    axios
+      .post("/login", {
+        user: {
+          username: username,
+          password: password
+        }
+      }, { withCredentials: true })
+      .then((res) => {
+        console.log("Register user")
+      })
+      .catch((error) => console.log(error))
+  }
+
   render () {
     return (
       <div>
         <h1>Login Form</h1>
         <div>
-          <label>Username: </label>
-          <input 
-            type="text"
-            name="user['username']"
-            value={this.state.username}
-            onChange={this.inputHandler}
-          />
-          <label>Password: </label>
-          <input 
-            type="password"
-            name="user['password']"
-            value={this.state.password}
-            onChange={this.inputHandler}
-          />
-          <button type="submit">Login</button>
+          <form onSubmit={this.handleSubmit}>
+            <label>Username: </label>
+            <input 
+              type="text"
+              name="username"
+              value={this.state.username}
+              onChange={this.inputHandler}
+            />
+            <label>Password: </label>
+            <input 
+              type="password"
+              name="password"
+              value={this.state.password}
+              onChange={this.inputHandler}
+            />
+            <button 
+            type="submit">Login</button>
+          </form>
         </div>
       </div>
     )
